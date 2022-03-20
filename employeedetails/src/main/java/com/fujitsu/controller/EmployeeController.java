a6119b990728f362545e63b73c7bfea3ee40f39d
pramijdhakle <pramij88@gmail.com>
2022-03-20 22:18:05 +0530

    New functionality added

--- employeedetails/src/main/java/com/fujitsu/controller/EmployeeController.java
+++ employeedetails/src/main/java/com/fujitsu/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.fujitsu.model.Employee;
 import com.fujitsu.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +23,11 @@ public class EmployeeController {
     @PostMapping("/getdatabyanyinput")
     public ResponseEntity<List<Employee>> getDataByAnyInput(@RequestBody Employee employee){
         return ResponseEntity.ok(employeeService.getDataByAnyInput(employee));
+    }
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome(){
+        return ResponseEntity.ok("welcome to csi");
     }
 
 
